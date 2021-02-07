81edebd200dd299f8626d7eaba9bcbb7cfaf8e0b
annjolie <annjolie.lourido@gmail.com>
2021-02-07 21:07:30 +0100

    agregando logica para tiempo restante

--- js/formatear_archivos.js
+++ js/formatear_archivos.js
@@ -1,3 +1,7 @@
+const todasLasLetras = "abcdefghijklmnñopqrstuvxyz";
+let data = "";
+let contador = 0;
+
 function leerArchivoTXT(archivo, callback)
 {
     var rawFile = new XMLHttpRequest();
@@ -19,7 +23,7 @@ function formatearTexto(texto) {
     const lineas = texto.split("\n");
     const listaPalabras = [];
     for (const linea of lineas) {
-        if (linea.length > 2 && /^[a-z]*,[^,]+$/.test(linea)) {
+        if (linea.length > 2 && (/^[a-z]*,[^,]+$|^[a-z]+$/.test(linea))) {
             const palabras = linea.replaceAll(" ", "").split(",");
             if (listaPalabras.indexOf(palabras[0]) < 0) {
                 // Si entra aqui es porque no tenemos esa palabra
@@ -31,13 +35,41 @@ function formatearTexto(texto) {
 
                 if (listaPalabras.indexOf(nuevaPalabra) < 0) {
                     // Si entra aqui es porque no tenemos esa palabra
-                    listaPalabras.push(palabras[0]);
+                    listaPalabras.push(nuevaPalabra);
                 }
             }
         }
     }
+    for (const palabra of listaPalabras) {
+        data += `INSERT INTO palabras_${palabra[0]} (palabra, puntaje) VALUE('${palabra}',${palabra.length - 2});\n`;
+    }
+    data += '\n-------------\n\n';
+    contador++;
+    console.log(contador, todasLasLetras.length);
+    if (contador === todasLasLetras.length) {
+        descargarArchivo(data, "letras.sql");
+    }
 }
 
-for (const letra of "abcdefghijklmnñopqrstuvxyz") {
+// Function to download data to a file
+function descargarArchivo(data, nombreArchivo) {
+    var file = new Blob([data], {type: "text/plain"});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, nombreArchivo);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = nombreArchivo;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+for (const letra of todasLasLetras) {
     leerArchivoTXT(letra, formatearTexto);
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,52 @@
-function actualizarScore(score) {
-    $("#score").html("score: " + score);
-}
+$(document).ready(function() {
+    var activo;
+    var tiempo_restante;
 
-function listaPalabras(palabras) {
-    $("#listaPalabras").html("");
-    for (var element of palabras) {
-        $("#listaPalabras").append("<li>" + element + "</li>");
+
+    function actualizarScore(score) {
+        $("#score").html("score: " + score);
     }
-}
+
+    function listaPalabras(palabras) {
+        $("#listaPalabras").html("");
+        for (var element of palabras) {
+            $("#listaPalabras").append("<li>" + element + "</li>");
+        }
+    }
+
+    function iniciarJuego() {
+        $("#buttonInicio").click(function() {
+            actualizarScore(0);
+            listaPalabras([]);
+            activo = true;
+            tiempo_restante = 180;
+            tiempoRestante();
+            setInterval(tiempoRestante, 1000);
+        });
+    }
+
+    function tiempoRestante() {
+        if (tiempo == "") {
+            $("#tiempo").html("-:--");
+        } else if (activo) {
+            var segundos = tiempo_restante % 60;
+            var minutos = Math.floor(tiempo_restante / 60);
+            if (segundos <= 9) {
+                segundos = "0" + segundos;
+            }
+            $("#tiempo").html("0" + minutos + ":" + segundos);
+            if (tiempo_restante == 0) {
+                activo = false;
+            }
+            tiempo_restante -= 1;
+        }
+
+    }
+
+    function pausarJuego() {
+
+    }
+
+    iniciarJuego();
+
+});
