a237e1f938fdc7a3a930303a30cdc92c06047e5e
annjolie <annjolie.lourido@gmail.com>
2021-02-07 12:59:49 +0100

    se agregan las fuentes "gordita" para el titulo y "Architects Daughter" para el cuerpo

--- js/formatear_archivos.js
+++ js/formatear_archivos.js
@@ -17,9 +17,23 @@ function leerArchivoTXT(archivo, callback)
 
 function formatearTexto(texto) {
     const lineas = texto.split("\n");
+    const listaPalabras = [];
     for (const linea of lineas) {
         if (linea.length > 2 && /^[a-z]*,[^,]+$/.test(linea)) {
-            
+            const palabras = linea.replaceAll(" ", "").split(",");
+            if (listaPalabras.indexOf(palabras[0]) < 0) {
+                // Si entra aqui es porque no tenemos esa palabra
+                listaPalabras.push(palabras[0]);
+            }
+            if (palabras[1]) {
+                // Si entra aqui es porque hay que revisar si tenemos esta palabra adicional
+                const nuevaPalabra = palabras[0].slice(0, palabras[0].lastIndexOf(palabras[1][0])) + palabras[1];
+
+                if (listaPalabras.indexOf(nuevaPalabra) < 0) {
+                    // Si entra aqui es porque no tenemos esa palabra
+                    listaPalabras.push(palabras[0]);
+                }
+            }
         }
     }
 }
